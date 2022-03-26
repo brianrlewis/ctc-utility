f2cd3a9353eea167d2f35aa9e41637a58302c53e
Brian Lewis <brlewis23@gmail.com>
2022-03-26 12:47:01 -0700

    Run prettier

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
-import { store } from "quasar/wrappers";
-import { createStore } from "vuex";
+import { store } from 'quasar/wrappers';
+import { createStore } from 'vuex';
 
 // import example from './module-example'
 
